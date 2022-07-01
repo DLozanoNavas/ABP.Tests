3576636b8757d41f4784f8a0efb9f63f3ab8c707
Daniel Lozano <d.lozanonavas@outlook.com>
2022-06-30 19:07:06 -0500

    Adds: Initial Files

---
+++ PluginTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Threading.Tasks;
+
+namespace FenixAlliance.ABP.Hub.Test
+{
+	[TestClass]
+	public class PluginTests
+	{
+		[TestMethod]
+		public async Task TestMethod1()
+		{
+
+		}
+	}
+}
