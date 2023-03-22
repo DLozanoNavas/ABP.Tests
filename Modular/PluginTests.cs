7d9cbf206e1df32c515fb863386764b008a0f763
Daniel Lozano <ceo@fenix-alliance.com>
2023-03-21 23:19:48 -0500

    [Adds] Functional Tests

---
+++ Modular/PluginTests.cs
@@ -0,0 +1,15 @@
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FenixAlliance.ABP.Tests.Modular
+{
+	[TestClass]
+	public class PluginTests
+	{
+		[TestMethod]
+		public async Task ListLoadedPlugins()
+		{
+
+		}
+	}
+}
---
+++ Templating/CodeCompilation.cs
@@ -0,0 +1,15 @@
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FenixAlliance.ABP.Tests.Templating
+{
+	[TestClass]
+	public class CodeCompilation
+	{
+		[TestMethod]
+		public async Task CompileCode()
+		{
+
+		}
+	}
+}
